4711153796b97230f781fe33e84eba9039637179
smeyk <oleksii_g@hotmail.com>
2023-10-30 10:17:49 +0000

    Deploying to gh-pages from @ smeyk/oleksii-js-hw-09@a46e438873829c0215a56897c009ca6ca0e7bed3 🚀

---
+++ 01-color-switcher.0a5823c6.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),a=null;t.addEventListener("click",(function(){a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.0a5823c6.js.map
