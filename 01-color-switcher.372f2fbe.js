6a4fa28a0b0dc1e105fd9be9c6757bc12af971da
smeyk <oleksii_g@hotmail.com>
2023-10-31 08:49:17 +0000

    Deploying to gh-pages from @ smeyk/oleksii-js-hw-09@1cfbb4733b740695d4567a61e764442034fb7bdb 🚀

--- 01-color-switcher.372f2fbe.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(()=>{clearInterval(a),e.disabled=!0,t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.372f2fbe.js.map
